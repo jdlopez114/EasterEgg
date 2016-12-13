f4829ba2c2476220a8f55906d699e707eca6e8a2
Josiel <jdlopez114@gmail.com>
2016-12-12 19:06:50 -0500

    added resources

---
+++ app/src/main/java/nyc/c4q/josiel/easteregg/BannerAdapter.java
@@ -0,0 +1,25 @@
+package nyc.c4q.josiel.easteregg;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+public class BannerAdapter extends RecyclerView.Adapter<BannerViewHolder> {
+
+    @Override
+    public BannerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(BannerViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/nyc/c4q/josiel/easteregg/BannerViewHolder.java
@@ -0,0 +1,11 @@
+package nyc.c4q.josiel.easteregg;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class BannerViewHolder extends RecyclerView.ViewHolder {
+
+    public BannerViewHolder(View itemView) {
+        super(itemView);
+    }
+}
