910091f166746aa0b577ea5338ff6005d028e829
Josiel <jdlopez114@gmail.com>
2016-12-15 22:32:42 -0500

    notifications

---
+++ app/src/main/java/nyc/c4q/josiel/easteregg/notifications/DataBaseActivity.java
@@ -0,0 +1,15 @@
+package nyc.c4q.josiel.easteregg.notifications;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import nyc.c4q.josiel.easteregg.R;
+
+
+public class DataBaseActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
